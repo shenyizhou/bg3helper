9adcb373a33ce5e74f831202302cd011539423da
shenyizhou <shenyizhou@corp.netease.com>
2024-07-03 17:26:53 +0800

    feat: 增加匹配规则

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @music/elint-preset-live
+ *
+ * npm：http://npm.hz.netease.com/package/@music/elint-preset-live
+ * gitlab：https://g.hz.netease.com/cloudmusic-frontend/lint/elint-preset-live
+ *
+ * 此文件自动生成，禁止修改！
+ * 此文件自动生成，禁止修改！
+ * 此文件自动生成，禁止修改！
+ */
+
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    singleQuote: true,
+    jsxBracketSameLine: true
+};
